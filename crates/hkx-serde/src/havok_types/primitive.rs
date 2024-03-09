0551285d2b6df8855f5b3def98eb25ac484dd1d0
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2024-03-10 03:17:48 +0900

    feat: change primitive manual pattern

--- crates/hkx-serde/src/havok_types/primitive.rs
+++ crates/hkx-serde/src/havok_types/primitive.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::{borrow::Cow, str::FromStr};
 
 /// Wrapper for retrieving the value that resides directly under the XML tag identified by the tagged enum.
 ///
@@ -10,13 +11,16 @@ use serde::{Deserialize, Serialize};
 /// ```xml
 /// <tag>2</tag>
 /// ```
-#[derive(Debug, Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+#[derive(Debug, Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Primitive<T> {
-    #[serde(rename = "$text")]
+    /// # Note
+    /// The fields are named because XML parsing failed in the form of `StructName(T)`.
     value: T,
 }
 
-/* impl<T> Serialize for Primitive<T>
+// # Why do we implement manually?
+// Space in $str and so on are not taken into account in $text, so you should switch to manual implementation if necessary.
+impl<T> Serialize for Primitive<T>
 where
     T: core::fmt::Display + Serialize,
 {
@@ -27,9 +31,9 @@ where
         // use Display trait by `to_string`
         serializer.serialize_str(&self.to_string())
     }
-} */
+}
 
-/* impl<'de, T> Deserialize<'de> for Primitive<T>
+impl<'de, T> Deserialize<'de> for Primitive<T>
 where
     T: std::default::Default + FromStr,
 {
@@ -53,25 +57,24 @@ where
             where
                 A: serde::de::MapAccess<'de>,
             {
+                use serde::de::Error;
                 let mut value = None;
 
                 while let Some(key) = map.next_key::<Cow<'_, str>>()? {
                     match key.as_bytes() {
                         b"$text" => {
                             let text: Cow<'_, str> = map.next_value()?;
-                            value = Some(text.parse().map_err(|_err| {
-                                serde::de::Error::custom("Failed to parse text as u64")
-                            })?);
+                            value = Some(
+                                text.parse()
+                                    .map_err(|_err| Error::custom("Failed to parse text as T"))?,
+                            );
                         }
-                        unknown => {
-                            let key = String::from_utf8_lossy(unknown);
-                            let content: Cow<'_, str> = map.next_value()?;
-                            tracing::warn!("Got unknown field. key: {key}, value: {content}",);
-                        }
+                        _unknown => {}
                     }
                 }
 
-                let value = value.unwrap_or_default();
+                let value =
+                    value.ok_or_else(|| Error::custom("Failed to get value in Primitive"))?;
 
                 Ok(Primitive { value })
             }
@@ -79,7 +82,7 @@ where
 
         deserializer.deserialize_map(InnerVisitor(std::marker::PhantomData))
     }
-} */
+}
 
 impl<T: core::fmt::Display> core::fmt::Display for Primitive<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -90,5 +93,54 @@ impl<T: core::fmt::Display> core::fmt::Display for Primitive<T> {
 impl<T> From<T> for Primitive<T> {
     fn from(value: T) -> Self {
         Self { value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn should_serialize() {
+        #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+        struct TestRoot<T>(T);
+
+        assert_eq!(
+            quick_xml::se::to_string(&TestRoot(Primitive::<i32> { value: 42 })).unwrap(),
+            "<TestRoot>42</TestRoot>"
+        );
+    }
+
+    #[test]
+    fn should_deserialize() {
+        let xml = r#"<AnyTagName>42</AnyTagName>"#;
+        let deserialized_primitive: Primitive<i32> = quick_xml::de::from_str(xml).unwrap();
+
+        assert_eq!(deserialized_primitive, Primitive::<i32> { value: 42 });
+    }
+
+    #[test]
+    fn should_serialize_with_space_value() {
+        #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+        struct TestRoot<T>(T);
+
+        assert_eq!(
+            quick_xml::se::to_string(&TestRoot(Primitive { value: "text text" })).unwrap(),
+            "<TestRoot>text text</TestRoot>"
+        );
+    }
+
+    #[test]
+    fn should_deserialize_with_space_value() {
+        let xml = r#"<AnyTagName>text text</AnyTagName>"#;
+        let deserialized_primitive: Primitive<String> = quick_xml::de::from_str(xml).unwrap();
+
+        assert_eq!(
+            deserialized_primitive,
+            Primitive {
+                value: "text text".into()
+            }
+        );
     }
 }
